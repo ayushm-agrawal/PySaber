546816a62c760c62a1a587763939d980065b6ed2
Ayush Manish Agrawal <ayush.agrawal7661@gmail.com>
2020-05-05 19:49:37 -0500

    feat: add ReLU and SoftMax activation functions

--- saber/nn/modules/activation.py
+++ saber/nn/modules/activation.py
@@ -3,6 +3,13 @@ import numpy as np
 
 
 class ReLU(Module):
+    """ ReLU or Rectified Linear Unit activation function.
+        Defined as the positive part of its argument
+
+        f(x) = x+ = max(0, x)
+
+    """
+
     def __init__(self):
         super(ReLU, self).__init__()
 
@@ -11,9 +18,23 @@ class ReLU(Module):
 
 
 class Sigmoid(Module):
+    """ Sigmoid Activation Function.
+
+        A sigmoid function is a bounded, differentiable, real function that is 
+        defined for all real input values and has a non-negative derivative at each point.
+
+        sig(x) =    1
+                ---------
+                1 + e^(-x)
+
+    """
+
     def __init__(self):
         super(Sigmoid, self).__init__()
 
     def forward(self, input):
-        # TODO: implement foward pass for Sigmoid Activation
-        pass
+        return (1/(1+np.exp(-input)))
+
+
+class Leaky_ReLU(Module):
+    raise NotImplementedError
